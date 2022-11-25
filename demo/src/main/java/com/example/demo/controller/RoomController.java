40861f0cdb5be11d480e5b17138ec0eb13ec8dcd
dawid <dawidbadora@gmail.com>
2022-11-25 14:56:37 +0100

    making a RoomController

--- demo/src/main/java/com/example/demo/controller/RoomController.java
+++ demo/src/main/java/com/example/demo/controller/RoomController.java
@@ -1,19 +1,49 @@
 package com.example.demo.controller;
 
 import com.example.demo.entity.Room;
-import com.example.demo.repository.RoomRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
+import com.example.demo.service.RoomService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
-@Controller
+@RestController
+@RequestMapping("/room")
 public class RoomController {
+    private final RoomService roomService;
 
+    public RoomController(RoomService roomService) {
+        this.roomService = roomService;
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Room>> getAllRooms(){
+        List<Room> rooms = roomService.findAllRooms();
+        return new ResponseEntity<>(rooms, HttpStatus.OK);
+    }
+
+    @GetMapping("/find/{id}")
+    public ResponseEntity<Room> getRoomById(@PathVariable("id") Long id){
+        Room room = roomService.findRoomById(id);
+        return new ResponseEntity<>(room, HttpStatus.OK);
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<Room> addRoom(@RequestBody Room room){
+        Room newRoom = roomService.addRoom(room);
+        return new ResponseEntity<>(newRoom, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<Room> updateRoom(@RequestBody Room room){
+        Room updateRoom = roomService.addRoom(room);
+        return new ResponseEntity<>(updateRoom, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> deleteRoom(@PathVariable("id") Long id){
+        roomService.deleteRoom(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
