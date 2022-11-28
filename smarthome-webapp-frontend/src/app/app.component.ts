b2e164844439eb753a91f0292ef43f49f9e4b654
dawid <dawidbadora@gmail.com>
2022-11-28 15:55:44 +0100

    working buttons

--- smarthome-webapp-frontend/src/app/app.component.ts
+++ smarthome-webapp-frontend/src/app/app.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 import { Room } from './room';
 import { RoomService } from './room.service';
 
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
